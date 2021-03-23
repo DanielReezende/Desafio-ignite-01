aaa4fa497953ff7a4c94d2df7894a8410cf84d5c
Daniel Rezende <daniel.zende@gmail.com>
2021-03-23 17:01:31 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,30 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== '') {
+      const task = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, task])
+
+      setNewTaskTitle('');
+    }
+    else {
+      alert('Por favor preencha o campo de título')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksCompleted = tasks.map(task => task.id === id ? { ...task, isComplete: !task.isComplete }: task);
+
+    setTasks(tasksCompleted)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
